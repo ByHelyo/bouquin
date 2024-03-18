2c1995e5a5eee58c3de86b3634af252172b7b7e8
Franck Hu <byfranckhu@outlook.com>
2024-03-18 16:24:25 +0100

    feat: dark mode

--- apps/browser/src/App.tsx
+++ apps/browser/src/App.tsx
@@ -1,5 +1,13 @@
+import { ThemeProvider } from "~/provider/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
+
 function App() {
-  return <div className="bg-red-500">Hello world!</div>;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ModeToggle />
+      <div className="bg-red-500">Hello world!</div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
