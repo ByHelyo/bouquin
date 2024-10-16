1ffea30348da0d2947ba94c7a037fbc81a597bd0
Franck Hu <byfranckhu@outlook.com>
2024-10-17 00:01:01 +0200

    build: configure manifest name, author, version

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,16 +1,29 @@
-import { defineConfig } from "wxt";
+import { defineConfig, UserManifest } from "wxt";
+import * as path from "path";
+import fs from "fs";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
-  manifest: {
-    action: {},
-    permissions: ["bookmarks"],
-    web_accessible_resources: [
-      {
-        resources: ["bouquin.html"],
-        matches: ["*://*/*"],
-      },
-    ],
+  manifest: () => {
+    const version = JSON.parse(
+        fs.readFileSync(path.resolve(__dirname, 'package.json')).toString(),
+    ).version;
+
+    const conf: UserManifest = {
+      name: "Bouquin",
+      author: "ByHelyo",
+      version: version,
+      action: {},
+      permissions: ["bookmarks"],
+      web_accessible_resources: [
+        {
+          resources: ["bouquin.html"],
+          matches: ["*://*/*"],
+        },
+      ],
+    };
+
+    return conf;
   },
 });
